4e66a64140633ed5a04650d8b47a7cf30d713ea9
Y.Nanthachai <thecoachingonline@gmail.com>
2023-04-18 13:25:32 +0000

    Hello Quantum World!

--- hello.py
+++ hello.py
@@ -1,19 +1,8 @@
-from qiskit import QuantumCircuit, Aer, execute
+import numpy as np
+from qiskit import QuantumCircuit
 
-# Create a quantum circuit with one qubit
-circuit = QuantumCircuit(1, 1)
+circuit = QuantumCircuit(3)
+circuit.h(0)
+circuit.cx(0, 1)
 
-# Apply the X gate (NOT gate) to the qubit
-circuit.x(0)
-
-# Measure the qubit and store the result in a classical bit
-circuit.measure(0, 0)
-
-# Simulate the circuit using the Qiskit Aer simulator
-backend = Aer.get_backend('qasm_simulator')
-job = execute(circuit, backend)
-result = job.result()
-
-# Print the result of the simulation
-counts = result.get_counts()
-print(counts)
+print('hello quantum world!')
